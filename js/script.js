96b9ab59747a33aa4cedc1558c0b60ab98d68537
solov <solovejko00@mail.ru>
2021-03-11 11:09:46 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ function loadCity(nameCity, id) {
         .catch(err => console.error(err));
 }
 
-function addNewCity(nameCity = undefined, load=false, id='id-1') {
+function addNewCity(nameCity = undefined, id='id-1') {
     let input_city = document.getElementById('add_city');
 
     nameCity = input_city.value;
@@ -47,6 +47,9 @@ function addNewCity(nameCity = undefined, load=false, id='id-1') {
         return;
 
     let url = `http://37.147.189.132:3000/weather/city?q=${nameCity}`;
+
+    id = Date.now().toString();
+    createEmptyElement(nameCity, id);
 
     fetch(url)
         .then(response => {return response.json()})
@@ -67,7 +70,8 @@ function addNewCity(nameCity = undefined, load=false, id='id-1') {
             })
                 .then(response => {return response.json()})
                 .then(commitsPost => {
-                    createEmptyElement(nameCity, commitsPost._id);
+                    document.getElementById(id).querySelector(".delete").setAttribute('onclick', "del('" + commitsPost._id + "')");
+                    document.getElementById(id).setAttribute('id', commitsPost._id);
 
                     let temp = ~~commits.main.temp;
                     let img = commits.weather[0].icon + '.png';
@@ -80,9 +84,25 @@ function addNewCity(nameCity = undefined, load=false, id='id-1') {
 
                     refactorElement(nameCity, temp, img, wind, cloud, press, hum, x, y, commitsPost._id);
                 })
-                .catch(err => console.error(err));
+                .catch(err => {
+                    alert('Произошла ошибка');
+                    console.error(err);
+                    delFast(id);
+                });
             })
-        .catch(err => console.error(err));
+        .catch(err => {
+            if (err === "401")
+                alert('Проблемы с ключом');
+            else
+                if (err === "404")
+                    alert('Нет информации об этом городе');
+                else
+                    if (err === "429")
+                        alert('Запросы в минуту превышают лимит бесплатного аккаунта');
+                    else
+                        alert('Произошла ошибка');
+            delFast(id);
+        });
 }
 
 function refactorElement(city='Moscow', temperature=5, img='weather.png',
@@ -127,8 +147,15 @@ function getLocation() {
                 refactorTopCity(nameCity, temp, img, wind, cloud, press, hum, x, y);
             })
             .catch(err => {
+                if (err === "401")
+                    alert('Проблемы с ключом');
+                else if (err === "404")
+                    alert('Нет информации об этом городе');
+                else if (err === "429")
+                    alert('Запросы в минуту превышают лимит бесплатного аккаунта');
+
                 console.error(err);
-            })
+            });
     }
 
     async function error({ message }) {
@@ -153,10 +180,17 @@ function getLocation() {
                 refactorTopCity(nameCity, temp, img, wind, cloud, press, hum, x, y);
             })
             .catch(err => {
-                console.error(err);
-            })
+                if (err === "401")
+                    alert('Проблемы с ключом');
+                else if (err === "404")
+                    alert('Нет информации об этом городе');
+                else if (err === "429")
+                    alert('Запросы в минуту превышают лимит бесплатного аккаунта');
 
-        console.error(message);
+                console.error(err);
+            });
+
+        alert('Пользователь запретил геолокацию');
     }
 }
 
@@ -194,101 +228,9 @@ function clearTop() {
 function createEmptyElement(city='Moscow', id='id-1') {
     let list = document.querySelector('.favorites');
 
-    let newFavorite = document.createElement('li');
-    newFavorite.setAttribute('class', 'favorite');
-    newFavorite.setAttribute('id', id);
-    let newH3 = document.createElement('h3');
-    newH3.textContent = city;
-
-    let newSpan = document.createElement('span');
-    newSpan.setAttribute('class', 'temperature');
-    newSpan.textContent = '-°C';
-    let newImg = document.createElement('img');
-    newImg.setAttribute('src', 'images/unknown.png');
-    let newButton = document.createElement('button');
-    newButton.setAttribute('type', 'button');
-    newButton.setAttribute('class', 'delete');
-    newButton.setAttribute('onclick', "del('" + newFavorite.getAttribute('id') + "')");
-
-    let newUl = document.createElement('ul');
-    newUl.setAttribute('class', 'weather');
-
-    let newLi1 = document.createElement('li');
-    newLi1.setAttribute('class', 'wind');
-    let newSpanBold1 = document.createElement('span');
-    newSpanBold1.setAttribute('class', 'bold');
-    newSpanBold1.textContent = 'Ветер';
-
-    let newSpanNormal1 = document.createElement('span');
-    newSpanNormal1.setAttribute('class', 'normal');
-    newSpanNormal1.textContent = '-';
-    newLi1.appendChild(newSpanBold1);
-    newLi1.appendChild(newSpanNormal1);
-
-    let newLi2 = document.createElement('li');
-    newLi2.setAttribute('class', 'cloud');
-    let newSpanBold2 = document.createElement('span');
-    newSpanBold2.setAttribute('class', 'bold');
-    newSpanBold2.textContent = 'Облачность';
-
-    let newSpanNormal2 = document.createElement('span');
-    newSpanNormal2.setAttribute('class', 'normal');
-    newSpanNormal2.textContent = '-';
-
-    newLi2.appendChild(newSpanBold2);
-    newLi2.appendChild(newSpanNormal2);
-
-    let newLi3 = document.createElement('li');
-    newLi3.setAttribute('class', 'pressure');
-    let newSpanBold3 = document.createElement('span');
-    newSpanBold3.setAttribute('class', 'bold');
-    newSpanBold3.textContent = 'Давление';
-
-    let newSpanNormal3 = document.createElement('span');
-    newSpanNormal3.setAttribute('class', 'normal');
-    newSpanNormal3.textContent = '-';
-
-    newLi3.appendChild(newSpanBold3);
-    newLi3.appendChild(newSpanNormal3);
-
-    let newLi4 = document.createElement('li');
-    newLi4.setAttribute('class', 'humidity');
-    let newSpanBold4 = document.createElement('span');
-    newSpanBold4.setAttribute('class', 'bold');
-    newSpanBold4.textContent = 'Влажность';
-
-    let newSpanNormal4 = document.createElement('span');
-    newSpanNormal4.setAttribute('class', 'normal');
-    newSpanNormal4.textContent = '-';
-
-    newLi4.appendChild(newSpanBold4);
-    newLi4.appendChild(newSpanNormal4);
-
-    let newLi5 = document.createElement('li');
-    newLi5.setAttribute('class', 'coord');
-    let newSpanBold5 = document.createElement('span');
-    newSpanBold5.setAttribute('class', 'bold');
-    newSpanBold5.textContent = 'Координаты';
-
-    let newSpanNormal5 = document.createElement('span');
-    newSpanNormal5.setAttribute('class', 'normal');
-    newSpanNormal5.textContent = '-';
-
-    newLi5.appendChild(newSpanBold5);
-    newLi5.appendChild(newSpanNormal5);
-
-    newUl.appendChild(newLi1);
-    newUl.appendChild(newLi2);
-    newUl.appendChild(newLi3);
-    newUl.appendChild(newLi4);
-    newUl.appendChild(newLi5);
-
-    newFavorite.appendChild(newH3);
-    newFavorite.appendChild(newSpan);
-    newFavorite.appendChild(newImg);
-    newFavorite.appendChild(newButton);
-    newFavorite.appendChild(newUl);
-
+    let newFavorite = tempCity.content.cloneNode(true);
+    newFavorite.querySelector('.favorite').setAttribute('id', id);
+    newFavorite.querySelector('.delete').setAttribute('onclick', "del('" + id + "')");
     list.appendChild(newFavorite);
 }
 
@@ -307,11 +249,31 @@ function del(idCity) {
         .then(res => {
             document.getElementById(idCity).style.display = "none";
         })
-        .catch(err => console.error(err));
+        .catch(err => {
+            if (err === "401")
+                alert('Проблемы с ключом');
+            else if (err === "404")
+                alert('Нет информации об этом городе');
+            else if (err === "429")
+                alert('Запросы в минуту превышают лимит бесплатного аккаунта');
+
+            console.error(err);
+        });
 }
 
-document.getElementById("add_city").addEventListener("keyup", function(event) {
+
+function delFast(idCity) {
+    document.getElementById(idCity).style.display = "none";
+}
+
+document.getElementById("myForm")
+    .addEventListener("submit", function(event) {
         event.preventDefault();
-        if (event.keyCode === 13)
-            document.getElementById("submit_city").click();
+        addNewCity();
+    });
+
+document.getElementById("butHeader")
+    .addEventListener("click", function(event) {
+        event.preventDefault();
+        update();
     });
